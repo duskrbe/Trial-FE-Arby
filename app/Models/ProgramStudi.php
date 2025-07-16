fdf21c67b3f144234fa495b9d508225a2d46c991
Arshi <fahrezzi@widianto.org>
2025-07-16 12:24:52 +0700

    Migrasi Prodi

---
+++ app/Models/ProgramStudi.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProgramStudi extends Model
+{
+    use HasFactory;
+
+    protected $table = 'program_studi';
+
+    protected $fillable = [
+        'ProdiID',
+        'Nama',
+        'Foto',
+        'Tahun_Berdiri',
+        'Deskripsi',
+    ];
+}
