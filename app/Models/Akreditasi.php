dc752a66afda51982cd65ccb2728c1d860653fa5
Arshi <fahrezzi@widianto.org>
2025-07-17 09:14:42 +0700

    Add: Menambahkan resource untuk Akreditasi

---
+++ app/Models/Akreditasi.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Akreditasi extends Model
+{
+    use HasFactory;
+
+    protected $table = 'akreditasi';
+
+    protected $fillable = [
+        'foto',
+        'deskripsi',
+        'prodi_id',
+    ];
+
+    public function prodi()
+    {
+        return $this->belongsTo(ProgramStudi::class, 'prodi_id');
+    }
+}
