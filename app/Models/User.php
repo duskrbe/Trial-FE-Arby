9f0550712b4f6ff55f674abf812fdd3644f3f9c0
Arshi <fahrezzi@widianto.org>
2025-07-26 09:24:32 +0700

    Error

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function prodi()
+    {
+        // Sesuaikan dengan nama foreign key dan primary key jika berbeda dari konvensi
+        return $this->belongsTo(ProgramStudi::class, 'prodi_id', 'id');
+    }
 }
